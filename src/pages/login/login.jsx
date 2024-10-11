7ea3dc6f5fe54af33cdb2b7f40c80f4e9967570c
RobertSilva2505 <robert.f.silva19@gmail.com>
2024-10-11 18:52:31 -0300

    Configuração Tela de Login
    Criação de tela de login e form com os dados de acesso.

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,5 @@
 import './login.css';
+import imagem from '../../assets/img_login.png';
 
 function Login() {
     return (
@@ -7,8 +8,24 @@ function Login() {
                 <h1 className='t1'>Portal do Suporte</h1>
                 <h2 className='t2'>Sistema Gálago</h2>
             </div>
-            <div className='form'>
-
+            <div className='fundo-form'>
+                <div>
+                    <figure>
+                        <img className='img' src={imagem} alt="imagem-login" />
+                    </figure>
+                </div>
+                <form action="" className='form'>
+                    <h1 className='t1 t1-t'>Login</h1>
+                    <p>E-mail:</p>
+                    <input type="email" className='inputs' autoComplete='email' placeholder='Digite seu e-mail' />
+                    <p>Senha:</p>
+                    <input type="password" className='inputs' placeholder='Digite sua senha' />
+                    <br />
+                    <a href="http://192.168.0.102:3000/login" target='_blank' className='link'>Esqueci minha senha</a>
+                    <button className='btn-login' type='button'>
+                            Acessar
+                    </button>
+                </form>
             </div>
         </div>
     )
