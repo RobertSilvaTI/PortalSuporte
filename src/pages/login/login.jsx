f9586afed2d590a46867c71d7d88991f63df8e0d
Robert Silva <robert.f.silva19@gmail.com>
2024-10-10 23:25:15 -0300

    Initial commit

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,17 @@
+import './login.css';
+
+function Login() {
+    return (
+        <div className='mestre'>
+            <div className='titulos'>
+                <h1 className='t1'>Portal do Suporte</h1>
+                <h2 className='t2'>Sistema Gálago</h2>
+            </div>
+            <div className='form'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login;
